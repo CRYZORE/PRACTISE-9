9c731822852d9031f02ec5aab50324afde3414ff
CRYZORE <cryzore@gmail.com>
2022-11-20 20:08:29 +0300

    v1.3 with api and documentation

--- pr9/api/serializers.py
+++ pr9/api/serializers.py
@@ -4,3 +4,4 @@ class ReplacerSerializer(serializers.Serializer):
     first_operand = serializers.CharField()
     result = serializers.CharField()
     operation = serializers.CharField(max_length=20)
+    
--- pr9/pr9/settings.py
+++ pr9/pr9/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'summary',
-    'api'
+    'api',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Replacer API',
+    'DESCRITION':'API that replaces text expression',
+    'VERSION': '1.2',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
