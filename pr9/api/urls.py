4d097dc620d441b178e390f2afb2b005081a11a9
CRYZORE <cryzore@gmail.com>
2022-11-28 19:47:13 +0300

    v1.3 with api and documentation

--- pr9/api/urls.py
+++ pr9/api/urls.py
@@ -1,16 +1,18 @@
-from django.urls import path
+from django.template.defaulttags import url
+from django.urls import path, include
 from . import views
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
+
 urlpatterns = [
     path('<str:s>', views.ReplacerView.as_view()),
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/", SpectacularAPIView.as_view(), name='schema.yml'),
     path(
         "docs/",
         SpectacularSwaggerView.as_view(
-            url_name="schema"
+            url_name='schema.yml'
          ),
-    name="swagger-ui",
+    name="swagger-ui"
     ),
 ]
--- pr9/pr9/settings.py
+++ pr9/pr9/settings.py
@@ -128,11 +128,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+   # 'SCHEMA_PATH_PREFIX': 'pr9/schema.yml',
 }
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Replacer API',
-    'DESCRITION':'API that replaces text expression',
+    'DESCRIPTION': 'API that replaces text expression',
     'VERSION': '1.2',
     'SERVE_INCLUDE_SCHEMA': False,
 }
